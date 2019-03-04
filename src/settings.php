5b12fbbcac85ed1f265829737ee3f99383c5249c
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-03-04 22:54:24 +0100

    add some settings for the twig renderer

--- src/settings.php
+++ src/settings.php
@@ -12,6 +12,18 @@ return [
             'name' => 'blog',
             'path' => __DIR__ . '/../var/log/app.log',
             'level' => \Monolog\Logger::DEBUG
-        ]
+        ],
+
+        // Twig
+        'twig' => [
+            'loader' => [
+                'paths' => [
+                    __DIR__ . '/../templates/'
+                ]
+            ],
+            'environment' => [
+                'cache' => __DIR__ . '/../var/cache/twig/'
+            ]
+        ],
     ]
 ];
