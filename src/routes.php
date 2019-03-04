1e305737979dddc8c350a96af853edacd8d7aacb
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-03-04 22:24:20 +0100

    add a fallback route that returns a 404 response

--- src/routes.php
+++ src/routes.php
@@ -1,19 +1,9 @@
 <?php
 declare(strict_types=1);
 
-use Slim\Http\Request;
-use Slim\Http\Response;
-
 $app->get('/', \App\Controller\Home::class);
 
 $app->get('/hello[/{name}]', \App\Controller\Hello::class);
 
-$app->get('/{page}', function (Request $request, Response $response, array $args) use ($app) {
-    $logger = $app->getContainer()->get('logger');
-    $logger->notice('Route Not Found', ['route' => $request->getUri()->getPath()]);
-    $name = $args['page'];
-    $response = $response->withStatus(404);
-    $response->getBody()->write("Page $name - Not Found");
-
-    return $response;
-});
+// Catch any undefined routes
+$app->get('[/{route:.*}]', \App\Controller\HttpNotFound::class);
