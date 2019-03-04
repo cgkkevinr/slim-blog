9a545a3aa3e6c2263b872fe590b8974cfb2ead80
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-03-04 22:53:47 +0100

    inject the renderer as a dependency into Hello controller

--- src/Controller/Hello.php
+++ src/Controller/Hello.php
@@ -3,14 +3,26 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Template\Renderer;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class Hello
 {
+    /**
+     * @var Renderer
+     */
+    private $renderer;
+
+    public function __construct(Renderer $renderer)
+    {
+        $this->renderer = $renderer;
+    }
+
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args)
     {
         $name = $args['name'] ?? 'stranger';
-        return $response->getBody()->write("Hello $name!");
+        $template = $this->renderer->render('welcome', ['name' => $name]);
+        return $response->getBody()->write($template);
     }
 }
