030728b311fff6b243e2b3d62f537e41d6a7e6f5
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-03-04 21:24:34 +0100

    example settings

---
+++ src/settings.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'displayErrorDetails' => true,
+    'addContentLengthHeader' => false,
+    'db' => [
+        'host' => 'localhost',
+        'user' => 'user',
+        'pass' => 'password',
+        'dbname' => 'example'
+    ]
+];
