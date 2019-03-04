ccdb72504974b3ffc8442a50d61975b2cb2eb62a
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-03-04 22:00:08 +0100

    back to the regular string notations

--- src/routes.php
+++ src/routes.php
@@ -11,7 +11,9 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     return $response;
 });
 
-$app->get('/{page}', function (Request $request, Response $response, array $args) {
+$app->get('/{page}', function (Request $request, Response $response, array $args) use ($app) {
+    $logger = $app->getContainer()->get('logger');
+    $logger->notice('Route Not Found', ['route' => $request->getUri()->getPath()]);
     $name = $args['page'];
     $response = $response->withStatus(404);
     $response->getBody()->write("Page $name - Not Found");
